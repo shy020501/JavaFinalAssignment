1ae3a206083514e4a7d74161576e2f1b28b10463
shy020501 <shy020501@naver.com>
2023-05-22 20:33:34 +0900

    create call program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] size = {600, 800};
+        String title = "Financial Ledger";
+        new LogInFrame(size, title);
     }
 }
