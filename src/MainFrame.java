0f66abc469b9ca33d3a6827b28d68a548a3e4d55
shy020501 <shy020501@naver.com>
2023-05-24 03:04:58 +0900

    create record generation functionality

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,10 +1,17 @@
 import com.sun.istack.internal.Nullable;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class MainFrame extends JFrame {
@@ -14,8 +21,48 @@ public class MainFrame extends JFrame {
     String userID; // Stores ID of the user so that the folder could be accessed
     private Color[] amountColor = {Color.BLUE, Color.RED}; // Stores color of saving/spending label
     private Color themeColor = new Color(240, 135, 132);
+    private int totalSpending = 0; // Stores total spending
+    private int totalSaving = 0; // Stores total saving
+    private int currentMonth = 11; // Stores the month the user is looking at
+    private int currentYear = 2023; // Stores the year the user is looking at
+    private String[] month = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
-    private JPanel createRecordPanel(String category, String description, int isSpending, int amount)
+    private JPanel createDatePanel(String date)
+    {
+        JPanel datePanel = new JPanel(); // Store panel for date
+        datePanel.setLayout(null); // Set layout to null
+        datePanel.setPreferredSize(new Dimension(frameSize[0] * 95 / 100, frameSize[1] / 15));
+        datePanel.setBorder(BorderFactory.createLineBorder(Color.black)); // Set border to black line
+        datePanel.setBackground(themeColor); // Set background to theme color
+
+        String ordinalExpression = "th"; // Store ordinal expression
+        if(!date.substring(0, 1).equals("1"))
+        {
+            // Setting
+            if(date.substring(1).equals("1")) ordinalExpression = "st";
+            else if (date.substring(1).equals("2")) ordinalExpression = "nd";
+            else if (date.substring(1).equals("3")) ordinalExpression = "rd";
+        }
+
+        if(date.substring(0, 1).equals("0")) date = date.substring(1);
+
+        JLabel dateLabel = new JLabel(month[currentMonth - 1] + " " + date + ordinalExpression); // Store label of current date
+        // Set bounds based on frame size
+        dateLabel.setBounds(
+                0,
+                0,
+                frameSize[0] * 95 / 100,
+                frameSize[1] / 15
+        );
+        dateLabel.setVerticalAlignment(JLabel.CENTER); // Set vertical alignment to center
+        dateLabel.setBorder(BorderFactory.createEmptyBorder(0 , frameSize[0] / 60 , 0 , 0)); // Set margin
+        dateLabel.setFont(new Font("Arial ", Font.BOLD, 18)); // Set font
+        dateLabel.setForeground(Color.white); // Set text color to white
+        datePanel.add(dateLabel);
+
+        return datePanel;
+    }
+    private JPanel createRecordPanel(String category, String description, int isSpending, int amount) // Create panel for user records
     {
         JPanel recordPanel = new JPanel(); // Store panel of each record
         recordPanel.setLayout(null); // Set layout to null
@@ -23,24 +70,42 @@ public class MainFrame extends JFrame {
         recordPanel.setBorder(BorderFactory.createLineBorder(Color.black));
         recordPanel.setBackground(Color.white); // Set background to while
 
-        if(category != null) {
-            JLabel categoryLabel = new JLabel(category); // Store label for displaying category
-            // Set bounds based on frame size
-            categoryLabel.setBounds(
-                    frameSize[0] / 60,
-                    0,
-                    frameSize[0] / 5,
-                    frameSize[1] / 15
-            );
-            categoryLabel.setVerticalAlignment(JLabel.CENTER); // Set vertical alignment to center
-            categoryLabel.setFont(new Font("Arial", Font.BOLD, 12)); // Set font
-            recordPanel.add(categoryLabel);
-        }
+        JButton editButton = new JButton(); // Button used to edit saved record
+        // Set bounds based on frame size
+        editButton.setBounds(
+                0,
+                0,
+                frameSize[0] * 95 / 100,
+                frameSize[1] / 15
+        );
+        // Set edit button transparent
+        editButton.setOpaque(false);
+        editButton.setContentAreaFilled(false);
+        editButton.setBorderPainted(false);
+        editButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Check click");
+            }
+        });
+        recordPanel.add(editButton);
+
+        JLabel categoryLabel = new JLabel(category); // Store label for displaying category
+        // Set bounds based on frame size
+        categoryLabel.setBounds(
+                frameSize[0] / 60,
+                0,
+                frameSize[0] / 5,
+                frameSize[1] / 15
+        );
+        categoryLabel.setVerticalAlignment(JLabel.CENTER); // Set vertical alignment to center
+        categoryLabel.setFont(new Font("Arial", Font.BOLD, 12)); // Set font
+        recordPanel.add(categoryLabel);
 
         JLabel descriptionLabel = new JLabel(description); // Store label for displaying description
         // Set bounds based on frame size
         descriptionLabel.setBounds(
-                frameSize[0] / 60 + frameSize[0] / 5,
+                frameSize[0] / 40 + frameSize[0] / 5,
                 0,
                 frameSize[0] * 2 / 5,
                 frameSize[1] / 15
@@ -185,18 +250,52 @@ public class MainFrame extends JFrame {
 
         constraints.fill = GridBagConstraints.BOTH; // The component of mainPanel fills the whole panel
 
-        for(int i = 0; i < 100; i++)
+
+        // Get directory of current year and month
+        String currentDir = "src/userInfo/" + ID + "/" + String.valueOf(currentYear) + "/" + String.valueOf(currentMonth) + "/";
+        File dir = new File(currentDir);
+        int count = 0;
+        if(dir.exists()) // If such directory exists
         {
-            constraints.gridx = 0; // Set to 0 as there's one component in a column
-            constraints.gridy = i; // Set to i as components would line up in a row
-            constraints.gridwidth = 1;
-            constraints.gridheight = 1;
+            String[] records = dir.list(); // Get all the text files(day) in the directory
+            Collections.reverse(Arrays.asList(records)); // Reverse the array so that the latest record would be shown on top
 
-            int isSpending = (i % 2 == 0 ? 0 : 1); // 0: saving, 1: spending
+            for(String record : records)
+            {
+                constraints.gridx = 0; // Set to 0 as there's one component in a column
+                constraints.gridy = count; // Set to i as components would line up in a row
+                constraints.gridwidth = 1;
+                constraints.gridheight = 1;
 
-            JPanel tempPanel = createRecordPanel(null, "Baseball game held in Olympic Stadium", 1, 100000);
+                count += 1; // Increment count by 1
 
-            mainPanel.add(tempPanel, constraints);
+                JPanel tempPanel = createDatePanel(record.substring(0, 2));
+                mainPanel.add(tempPanel, constraints);
+
+                File dailyRecord = new File(currentDir + record); // Stores each daily text file
+                try{
+                    Scanner fileReader = new Scanner(dailyRecord); // Create scanner for reading text file
+                    while(fileReader.hasNextLine()){
+                        String rawData = fileReader.nextLine(); // Read next line from text file
+                        String[] data = rawData.split(" "); // Split data inside text file based on space
+
+                        String category = "Saving";
+                        if(data[0].equals("1")) category = data[3]; // If data is spending record, fill in category
+
+                        constraints.gridx = 0; // Set to 0 as there's one component in a column
+                        constraints.gridy = count; // Set to i as components would line up in a row
+                        constraints.gridwidth = 1;
+                        constraints.gridheight = 1;
+
+                        count += 1; // Increment count by 1
+                        tempPanel = createRecordPanel(category, data[1], Integer.parseInt(data[0]), Integer.parseInt(data[2]));
+                        mainPanel.add(tempPanel, constraints);
+                    }
+                } catch (FileNotFoundException ex) {
+                    System.out.println("File not found!");
+                    ex.printStackTrace();
+                }
+            }
         }
 
         JScrollPane scrollPane = new JScrollPane(mainPanel, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
@@ -292,10 +391,9 @@ public class MainFrame extends JFrame {
         setLayout(null);
         setVisible(true);
     }
-
     public static void main(String[] args) {
         int[] size = {600, 800};
         String title = "Financial Ledger";
-        new MainFrame(size, title, "ID");
+        new MainFrame(size, title, "shy020501");
     }
 }
