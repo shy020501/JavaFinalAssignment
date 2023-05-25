88c617ccd9eb284e568007c75758fa7880bbfc41
shy020501 <shy020501@naver.com>
2023-05-25 12:31:01 +0900

    create userInfo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
+        File userInfo = new File("src/userInfo/");
+
+        if(!userInfo.exists())
+        {
+            try {
+                Files.createDirectories(Paths.get("src/userInfo/"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         int[] size = {600, 800};
         String title = "Financial Ledger";
         new LogInFrame(size, title);
