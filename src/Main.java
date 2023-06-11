a647d0cbd8507d57fdce59d09063d8cf33cce566
shy020501 <shy020501@naver.com>
2023-06-11 21:47:07 +0900

    add comment

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) {
         File userInfo = new File("src/userInfo/");
 
-        if(!userInfo.exists())
+        if(!userInfo.exists()) // For the first launching of the program, userInfo directory wouldn't exist
         {
             try {
-                Files.createDirectories(Paths.get("src/userInfo/"));
+                Files.createDirectories(Paths.get("src/userInfo/")); // Create userInfo file
             } catch (IOException e) {
                 e.printStackTrace();
             }
